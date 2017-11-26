80b2c2799773e54cc6409f70ce4e862549af84c2
Philip Hopkins <phopkins@caltech.edu>
2017-11-26 07:33:50 -0800

    cleaned up subfolder for user guide

---
+++ gravity/cbe_integrator.c
@@ -0,0 +1,22 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <gsl/gsl_math.h>
+#include "../allvars.h"
+#include "../proto.h"
+#include "../kernel.h"
+
+
+/*! \file cbe_integrator.c
+ *  \brief routines needed for CBE integrator implementation
+ *         This file was written by Phil Hopkins (phopkins@caltech.edu) for GIZMO.
+ */
+
+#define CBE_DEBUG
+
+
+// moment ordering convention: 0, x, y, z, xx, yy, zz, xy, xz, yz
+
+
